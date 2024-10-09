7fe658bdfe86e113d60889436962725521dc8521
M <41967202+swzjustin@users.noreply.github.com>
2024-10-09 21:50:43 +0100

    !

--- tables.js
+++ tables.js
@@ -147,6 +147,15 @@ const generateMainTable = async (tableName, token) => {
         input.id = record.name
         label.setAttribute('for', record.name)
         label.onclick = (e) => {
+            const ths = theadtr.querySelectorAll('th')
+            for(const t of ths){
+                const input = t.querySelector('input')
+                if(!input.checked){
+                    const l = t.querySelector('label')
+                    label.classList.remove('desc')
+                    label.classList.remove('asc')
+                }
+            }
             console.log('clicked')
             e.stopPropagation()
             if (label.classList.contains('asc')) {

43c1bb15c96c9499c17baa035cca45b39463102c
M <41967202+swzjustin@users.noreply.github.com>
2024-10-10 19:50:12 +0100

    !

--- tables.js
+++ tables.js
@@ -36,7 +36,7 @@ const generateMainTable = async (tableName, token) => {
     if (!records) {
         return
     }
-    records = [{ columnName: 'created', name: 'created' }, ... records.sort((a, b) => {return posA - posB;})]
+    records = [{ columnName: 'created', name: 'created' }, ... records.sort((a, b) => {return a - b;})]
     document.getElementById('tableToShow')?.remove()
     const table = t.cloneNode(true)
     table.removeAttribute('hidden')

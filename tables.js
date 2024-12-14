b36713585e8d613aaa0b404acd19abbb5fcda21e
M <41967202+swzjustin@users.noreply.github.com>
2024-12-14 01:10:54 +0100

    !

--- tables.js
+++ tables.js
@@ -572,7 +572,7 @@ const generateMainTable = async (tableName, token) => {
         console.log(JSON.stringify(results))
         for (let i = 0; i < results.length; i++) {
             const r = results[i]
-            generateRecord({ id: r[0],shortId:p[i].shortId,created:p[i].created }, true, total + i)
+            generateRecord({ id: r[0],shortId:p[i].shortId,created:p[i].created }, false, total + i)
         }
         total += results.length
         runScript1()
